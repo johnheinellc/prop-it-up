f31c94db5276befa505817baa4e22eb9ac40f330
johnheinellc <johnheinellc@gmail.com>
2022-03-09 18:08:57 -0500

    first commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class PersonCard extends Component {
+    render() {
+        return (
+            <div>
+
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+                <hr/>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
